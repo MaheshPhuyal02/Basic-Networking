52bc833b8888e79d242a339e85c82cc65374c22d
Maheshphuyal62 <maheshphuyal62@gmail.com>
2021-11-26 10:52:41 +0545

    New

---
+++ main.py
@@ -0,0 +1,9 @@
+import requests
+
+print("Welcome to basic request and get program")
+print("****************************************")
+link = input("Paste your link here: n\ ")
+r = requests.get(link)
+print(r.status_code)
+print(r.headers)
+print(r.content)
