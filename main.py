96c30cd8c18917edd25538859d505d374cf9c2d0
MaheshPhuyal02 <94818265+MaheshPhuyal02@users.noreply.github.com>
2021-11-26 10:55:37 +0545

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import requests
 
 print("Welcome to basic request and get program")
 print("****************************************")
-link = input("Paste your link here: n\ ")
+link = input("Paste your link here: ")
 r = requests.get(link)
 print(r.status_code)
 print(r.headers)
